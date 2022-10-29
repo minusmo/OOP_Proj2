5f6148d818d6563ed5060e9a8085cc121f94c349
hojooneum <bldolphin96@gmail.com>
2022-10-29 21:27:33 +0900

    update implementation to use digits as string type

--- prob1/inf_int.cpp
+++ prob1/inf_int.cpp
@@ -17,9 +17,8 @@
 inf_int::inf_int() {
 	this->length = 1;
 	this->thesign = true;
-	this->digits = new char[2];
-	this->digits[0] = '0';
-	this->digits[1] = '\0';
+	this->digits = string();
+	this->digits.push_back('0');
 }
 
 inf_int::inf_int(int n) {
@@ -47,9 +46,14 @@ inf_int::inf_int(int n) {
 	else {
 		buffer[i] = '\0';
 
-		this->digits = new char[i + 1];
+		this->digits = string();
 		this->length = i;
-		strcpy(this->digits, buffer);
+
+		int j;
+		for (j = 0; j < i + 1; j++) {
+			if (buffer[j] == '\0') break;
+			this->digits.push_back(buffer[j]);
+		}
 	}
 }
 
@@ -61,41 +65,39 @@ inf_int::inf_int(const char* str) {
 		this->thesign = true;
 	}
 
-	int strLen = sizeof(str) / sizeof(char);
+	size_t strLen = strlen(str);
 	int i = 0;
-	if (this->thesign == false) {
-		for (; i < strLen-1; i++) {
-			this->digits[i] = str[strLen - 1 - i];
-		}
+	this->digits = string();
+	for (; i < strLen; i++) {
+		this->digits.push_back(str[strLen - 1 - i]);
 	}
-	else {
-		for (; i < strLen; i++) {
-			this->digits[i] = str[strLen - 1 - i];
-		}
-	}
-	this->length = i + 1;
+
+	this->length = (unsigned int)strLen;
 }
 
 inf_int::inf_int(const inf_int& ex_inf_int) {
-	this->digits = new char[ex_inf_int.length + 1];
+	this->digits = string();
+	unsigned int i;
+	for (auto digit : ex_inf_int.digits) {
+		this->digits.push_back(digit);
+	}
 
-	strcpy(this->digits, ex_inf_int.digits);
 	this->length = ex_inf_int.length;
 	this->thesign = ex_inf_int.thesign;
 }
 
 inf_int::~inf_int() {
-	delete digits;
+
 }
 
 inf_int& inf_int::operator=(const inf_int& ex_inf_int)
 {
-	if (this->digits) {
-		delete this->digits;		// 이미 문자열이 있을 경우 제거.
+	this->digits = string();
+	unsigned int i;
+	for (auto digit : ex_inf_int.digits) {
+		this->digits.push_back(digit);
 	}
-	this->digits = new char[ex_inf_int.length + 1];
 
-	strcpy(this->digits, ex_inf_int.digits);
 	this->length = ex_inf_int.length;
 	this->thesign = ex_inf_int.thesign;
 
@@ -105,7 +107,7 @@ inf_int& inf_int::operator=(const inf_int& ex_inf_int)
 bool operator==(const inf_int& a, const inf_int& b)
 {
 	// we assume 0 is always positive.
-	if ((strcmp(a.digits, b.digits) == 0) && a.thesign == b.thesign)	// 부호가 같고, 절댓값이 일치해야함.
+	if ((a.digits == b.digits) && a.thesign == b.thesign)	// 부호가 같고, 절댓값이 일치해야함.
 		return true;
 	return false;
 }
@@ -153,13 +155,50 @@ inf_int operator+(const inf_int& a, const inf_int& b)
 	unsigned int i;
 
 	if (a.thesign == b.thesign) {	// 이항의 부호가 같을 경우 + 연산자로 연산
-		for (i = 0; i < a.length; i++) {
-			c.Add(a.digits[i], i + 1);
-		}
-		for (i = 0; i < b.length; i++) {
-			c.Add(b.digits[i], i + 1);
-		}
+		c.digits = string();
+		if (a.length < b.length) {
+			int carry = 0;
+			for (int i = 0; i < (int)a.length; i++)
+			{
+				int sum = ((a.digits[i] - '0') + (b.digits[i] - '0') + carry);
+				c.digits.push_back(sum % 10 + '0');
 
+				carry = sum / 10;
+			}
+
+			for (int i = a.length; i < (int)b.length; i++)
+			{
+				int sum = ((b.digits[i] - '0') + carry);
+				c.digits.push_back(sum % 10 + '0');
+				carry = sum / 10;
+			}
+
+			if (carry)
+				c.digits.push_back(carry + '0');
+		}
+		else {
+			int carry = 0;
+			for (int i = 0; i < (int)b.length; i++)
+			{
+				int sum = ((a.digits[i] - '0') + (b.digits[i] - '0') + carry);
+				c.digits.push_back(sum % 10 + '0');
+
+				carry = sum / 10;
+			}
+
+			for (int i = b.length; i < (int)a.length; i++)
+			{
+				int sum = ((a.digits[i] - '0') + carry);
+				c.digits.push_back(sum % 10 + '0');
+				carry = sum / 10;
+			}
+
+			if (carry)
+				c.digits.push_back(carry + '0');
+		}
+		
+
+		c.length = (unsigned int)c.digits.length();
 		c.thesign = a.thesign;
 		
 		return c;
@@ -175,52 +214,98 @@ inf_int operator+(const inf_int& a, const inf_int& b)
 inf_int operator-(const inf_int& a, const inf_int& b)
 {
 	// to be filled
-	inf_int c,d;
-	unsigned int i;
-
-	if (a.thesign != b.thesign) {	// if thesign is different, unify thesign and +
-		d = b;
-		d.thesign = a.thesign;
-		c = a + d;
-		return c;
-	}
-	else {	// if thesign is same, substitute absolute value and decide thesign. always substitute from bigger to smaller number
-		if (absLess(a, b)) {
-			if (a.thesign) {
-				c.thesign = false;
-			}
-			else {
-				c.thesign = true;
-			}
-			for (i = 0; i < b.length; i++) {
-				c.Add(b.digits[i], i + 1);
-			}
+	inf_int c;
+	
+	if (absLess(a, b)) {
+		// subtract a from b
+		if (b.thesign == a.thesign) {
+			c.digits = string();
+			int carry = 0;
+			unsigned int i;
 			for (i = 0; i < a.length; i++) {
-				c.Sub(a.digits[i], i + 1);
+				int sub = ((b.digits[i] - '0') - (a.digits[i] - '0') - carry);
+				if (sub < 0) {
+					sub = sub + 10;
+					carry = 1;
+				}
+				else {
+					carry = 0;
+				}
+				c.digits.push_back(sub + '0');
 			}
+			for (i = a.length; i < b.length; i++) {
+				int sub = ((b.digits[i] - '0') - carry);
+				if (sub < 0) {
+					sub = sub + 10;
+					carry = 1;
+				}
+				else {
+					carry = 0;
+				}
+				c.digits.push_back(sub + '0');
+			}
+
+			c.length = (unsigned int)c.digits.length();
+			c.thesign = b.thesign;
+			return c;
 		}
-		else if (strcmp(a.digits, b.digits)) {
+		else {
+			inf_int d = a;
+			d.thesign = b.thesign;
+			c = b + d;
+			return c;
+		}
+	}
+	else if (a.digits == b.digits) {
+		// when two numbers are the same
+		if (a.thesign == b.thesign) {
 			c = inf_int();
 		}
 		else {
-			if (a.thesign) {
-				c.thesign = true;
-			}
-			else {
-				c.thesign = false;
-			}
-			for (i = 0; i < a.length; i++) {
-				c.Add(a.digits[i], i + 1);
-			}
-			for (i = 0; i < b.length; i++) {
-				c.Sub(b.digits[i], i + 1);
-			}
+			c = inf_int(2) * a;
+			c.thesign = a.thesign;
 		}
-
 		return c;
 	}
+	else {
+		// subtract b from a
+		if (a.thesign == b.thesign) {
+			c.digits = string();
+			int carry = 0;
+			for (unsigned int i = 0; i < b.length; i++) {
+				int sub = ((a.digits[i] - '0') - (b.digits[i] - '0') - carry);
+				if (sub < 0) {
+					sub = sub + 10;
+					carry = 1;
+				}
+				else {
+					carry = 0;
+				}
+				c.digits.push_back(sub + '0');
+			}
+			for (unsigned int i = b.length; i < a.length; i++) {
+				int sub = ((a.digits[i] - '0') - carry);
+				if (sub < 0) {
+					sub = sub + 10;
+					carry = 1;
+				}
+				else {
+					carry = 0;
+				}
+				c.digits.push_back(sub + '0');
+			}
 
-
+			c.length = (unsigned int)c.digits.length();
+			c.thesign = a.thesign;
+			return c;
+		}
+		else {
+			inf_int d = b;
+			d.thesign = a.thesign;
+			c = a + d;
+			return c;
+		}
+	}
 }
 
 inf_int operator*(const inf_int& a, const inf_int& b)
@@ -237,28 +322,25 @@ inf_int operator*(const inf_int& a, const inf_int& b)
 	else {
 		c.thesign = false;
 	}
-	c.length = a.length + b.length;
-	c.digits = new char[c.length + 1];
-	
-	for (unsigned int i = 0; i < a.length; i++) {
-		for (unsigned int j = 0; j < b.length; j++) {
-			if (c.digits[i + j] != '0') c.digits[i + j] = '0';
-			int n = (a.digits[i] - '0') * (b.digits[j] - '0') + c.digits[i + j];
+
+	c.digits = string(a.length+b.length,0);
+
+	for (int i = 0; i < (int)a.length; i++) {
+		for (int j = 0; j < (int)b.length; j++) {
+			int n = c.digits[i + j] + (a.digits[i] - '0') * (b.digits[j] - '0');
 			c.digits[i + j] = n % 10;
-			c.digits[i + j + 1] += n / 10;
+			c.digits[i + j + 1] += n/10;
 		}
 	}
-	for (unsigned int i = 0; i < c.length; i++) {
-		c.digits[i] += '0';
+	
+	for (int k = 0; k < c.digits.length();k++) {
+		c.digits[k] += '0';
 	}
-	if (c.digits[c.length-1] == '0') {
-		c.digits = (char*)realloc(c.digits, c.length);
-		c.digits[c.length - 1] = '\0';
-		c.length -= 1;
+	if (c.digits[c.digits.length() - 1] == '0') {
+		c.digits = c.digits.substr(0, c.digits.length() - 1);
 	}
-	else {
-		c.digits[c.length] = '\0';
-	}
+
+	c.length = (unsigned int)c.digits.length();
 	return c;
 }
 
@@ -279,56 +361,18 @@ ostream& operator<<(ostream& out, const inf_int& a)
 void inf_int::Add(const char num, const unsigned int index)	// a의 index 자리수에 n을 더한다. 0<=n<=9, ex) a가 391일때, Add(a, 2, 2)의 결과는 411
 {
 	if (this->length < index) {
-		this->digits = (char*)realloc(this->digits, index + 1);
-
-		if (this->digits == NULL) {		// 할당 실패 예외처리
-			cout << "Memory reallocation failed, the program will terminate." << endl;
-
-			exit(0);
-		}
-
 		this->length = index;					// 길이 지정
-		this->digits[this->length] = '\0';	// 널문자 삽입
+		this->digits.push_back(num % 10 + '0');
 	}
-
-	if (this->digits[index - 1] < '0') {	// 연산 전에 '0'보다 작은 아스키값인 경우 0으로 채움. 쓰여지지 않았던 새로운 자리수일 경우 발생
-		this->digits[index - 1] = '0';
-	}
-
-	this->digits[index - 1] += num - '0';	// 값 연산
-
-
-	if (this->digits[index - 1] > '9') {	// 자리올림이 발생할 경우
-		this->digits[index - 1] -= 10;	// 현재 자릿수에서 (아스키값) 10을 빼고
-		Add('1', index + 1);			// 윗자리에 1을 더한다
-	}
-}
-
-void inf_int::Sub(const char num, const unsigned int index) {// substitute num at index of a
-	// 항상 절대값이 더 큰 것에서 작은 것을 빼므로 피연산자(RHS)의 인덱스가 길이를 넘어갈 일이 없다.
-
-	if (this->digits[index - 1] < '0') {	// 연산 전에 '0'보다 작은 아스키값인 경우 0으로 채움. 쓰여지지 않았던 새로운 자리수일 경우 발생
-		this->digits[index - 1] = '0';
-	}
-
-	this->digits[index - 1] -= num + '0';	// 값 연산. 0-9의 값의 차를 구하고 베이스 값 '0'을 더한다.
-
-
-	if (this->digits[index - 1] < '0') {	// 자리내림이 발생할 경우
-		this->digits[index - 1] += 10;	// 현재 자릿수에서 (아스키값) 10을 더하고
-		Sub('1', index + 1);			// 윗자리에 1을 뺀다
-	}
-
-	if (this->digits[index - 1] == '0' && index == this->length) {
-		// 가장 큰 자리수의 숫자가 같은 상황에서 뺀 경우
-		this->digits = (char*)realloc(this->digits, index);
-		if (this->digits == NULL) {		// 할당 실패 예외처리
-			cout << "Memory reallocation failed, the program will terminate." << endl;
-
-			exit(0);
+	else {
+		int sum = num - '0' + this->digits[index-1] - '0';
+		if (sum > 9) {
+			this->digits[index - 1] = sum % 10 + '0';
+			Add('1', index + 1);
 		}
-		this->length -= 1;
-		this->digits[this->length] = '\0';
+		else {
+			this->digits[index - 1] = sum + '0';
+		}
 	}
 }
 
@@ -347,16 +391,15 @@ bool absLess(const inf_int& a, const inf_int& b) {
 		return false;
 	}
 	else {
-		for (int i = 0; i < a.length; i++)
+		for (unsigned int i = 0; i < a.length; i++)
 		{
 			if (a.digits[i] > b.digits[i]) {
 				return false;
 			}
-			else {
-				continue;
+			else if (a.digits[i] < b.digits[i]) {
+				return true;
 			}
 		}
-		if (strcmp(a.digits, b.digits)) return false;
-		return true;
+		return false;
 	}
 }
